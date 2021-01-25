81d773df3ed99aa3888d4e6d38048544e78ddeb2
Andre Morpa <andremorpa@gmail.com>
2021-01-25 16:06:41 +0000

    Add logo

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import LogoStrapi from "../../../../assets/images/traveler.svg";
+import Img from "./Img";
+
+const Logo = () => <Img src={LogoStrapi} alt="Traveler" />;
+
+export default Logo;
